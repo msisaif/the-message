3a8eb4f6f73f7e6b8dbc2ed36b75cf20b3f325d3
msisaif <msi132043@gmail.com>
2021-11-19 00:23:38 +0600

    Model & Migration

--- database/migrations/2021_11_18_170637_create_reciters_table.php
+++ database/migrations/2021_11_18_170637_create_reciters_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTranslationsTable extends Migration
+class CreateRecitersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateTranslationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('translations', function (Blueprint $table) {
+        Schema::create('reciters', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedTinyInteger('tafsir_id');
-            $table->unsignedSmallInteger('ayah_number');
-            $table->text('text');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -31,6 +29,6 @@ class CreateTranslationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('translations');
+        Schema::dropIfExists('reciters');
     }
 }
--- database/migrations/2021_11_18_175945_create_quran_audio_table.php
+++ database/migrations/2021_11_18_175945_create_quran_audio_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTranslationsTable extends Migration
+class CreateQuranAudioTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateTranslationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('translations', function (Blueprint $table) {
+        Schema::create('quran_audio', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedTinyInteger('tafsir_id');
+            $table->unsignedBigInteger('reciter_id');
             $table->unsignedSmallInteger('ayah_number');
-            $table->text('text');
+            $table->string('url');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -31,6 +31,6 @@ class CreateTranslationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('translations');
+        Schema::dropIfExists('quran_audio');
     }
 }
