2441df8d4e123d1af46aa418f4075178f19804a5
msisaif <msi132043@gmail.com>
2022-06-15 16:47:10 +0600

    join

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,7 +1,7 @@
 <template>
     <button
         :type="type"
-        class="inline-flex items-center px-4 py-2 bg-sky-400 border border-transparent font-semibold text-base text-white uppercase tracking-widest hover:bg-sky-500 active:bg-sky-500 focus:outline-none focus:border-sky-500 focus:shadow-outline-sky transition ease-in-out duration-150"
+        class="inline-flex items-center px-4 py-2 bg-brand-primary border border-transparent font-semibold text-base text-white uppercase tracking-widest hover:bg-brand-primary/90 active:bg-brand-primary/90 focus:outline-none focus:border-brand-primary/90 focus:shadow-outline-sky transition ease-in-out duration-150"
     >
         <slot />
     </button>
--- resources/js/Layouts/Account.vue
+++ resources/js/Layouts/Account.vue
@@ -6,7 +6,7 @@
                 :href="route('join')"
                 class="px-3 md:px-6 py-0.5 md:py-1.5 border-2 border-brand-primary text-brand-primary hover:bg-brand-primary hover:text-white font-bold"
             >
-                Join
+                লগইন
             </Link>
         </div>
         <div v-else class="relative">
@@ -197,7 +197,7 @@
                                 d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                             />
                         </svg>
-                        <span>Log Out</span>
+                        <span>লগআউট</span>
                     </dropdown-link>
                 </template>
             </dropdown>
